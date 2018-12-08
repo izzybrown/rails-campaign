fce19f425d22ed7812555ebd4ab4f0e234c59af0
Izzy Brown <izzybrown@hotmail.co.uk>
2018-12-08 10:10:01 +0000

    added concepts and index.rb

---
+++ app/concepts/campaign/operations/index.rb
@@ -0,0 +1,9 @@
+class Campaign < ActiveRecord::Base
+  class Index < Trailblazer::Operation
+    step :model!
+
+    def model!(options, *)
+      options['model'] = ::Campaign.all
+    end
+  end
+end
