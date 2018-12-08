356c0de1a4b002ae4bf441d7f168d448512c7e55
Izzy Brown <izzybrown@hotmail.co.uk>
2018-12-08 10:18:09 +0000

    create

---
+++ app/concepts/campaign/operations/create.rb
@@ -0,0 +1,23 @@
+class EventCampaign < ActiveRecord::Base
+  class Create < Trailblazer::Operation
+    extend Contract::DSL
+
+    contract do
+      property :title
+      property :startdate
+      property :budget
+      property :brief
+
+      validates :title, presence: true
+      validates :startdate, presence: true, startdate_in_future: true
+      validates :budget, presence: true
+      validates :brief, presence: true
+    end
+
+    step Model(Campaign, :new)
+    step Contract::Build()
+    step Contract::Validate(key: :eventcampaign)
+    step :persist!
+    step :notify!
+  end
+end
